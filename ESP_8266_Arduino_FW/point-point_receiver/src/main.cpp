25848a30d723e25de689620efa8a11c5cef53704
Inżynier Domu <inzynier.domu@gmail.com>
2025-03-11 12:14:11 +0100

    fix for esp8266

--- ESP_8266_Arduino_FW/point-point_receiver/src/main.cpp
+++ ESP_8266_Arduino_FW/point-point_receiver/src/main.cpp
@@ -1,27 +1,21 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <esp_now.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 
 struct esp_now_message
 {
-  char text[32];
-  int value;
-  bool flag;
+  double value;
 };
 
 esp_now_message message_content;
 
-void print_received_msg(const uint8_t* mac, const uint8_t* incoming_data, int len)
+void print_received_msg(uint8_t* mac, uint8_t* incoming_data, uint8_t len)
 {
   memcpy(&message_content, incoming_data, sizeof(message_content));
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("text: ");
-  Serial.println(message_content.text);
   Serial.print("value: ");
   Serial.println(message_content.value);
-  Serial.print("flag: ");
-  Serial.println(message_content.flag);
   Serial.println("-----------");
 }
 
@@ -31,7 +25,7 @@ void setup()
 
   WiFi.mode(WIFI_STA);
 
-  if (esp_now_init() != ESP_OK)
+  if (esp_now_init() != ERR_OK)
   {
     Serial.println("Error initializing ESP-NOW");
     return;
--- Wrapper/point-point_receiver/src/main.cpp
+++ Wrapper/point-point_receiver/src/main.cpp
@@ -12,8 +12,13 @@ struct esp_now_message
 };
 
 esp_now_message message_content;
+uint8_t mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 
+#ifdef ESP8266
+void print_received_msg(uint8_t* mac, uint8_t* incoming_data, uint8_t len)
+#elif ESP32
 void print_received_msg(const uint8_t* mac, const uint8_t* incoming_data, int len)
+#endif
 {
   memcpy(&message_content, incoming_data, sizeof(message_content));
   Serial.print("Bytes received: ");
@@ -23,20 +28,23 @@ void print_received_msg(const uint8_t* mac, const uint8_t* incoming_data, int le
   Serial.println("-----------");
 }
 
-
 void setup()
 {
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
+#ifdef ESP8266
+  WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+#elif ESP32
+  WiFi.mode(WIFI_MODE_STA);
+#endif
 
-  if (esp_now_init() != ESP_OK)
+  if (ESPNow.init() != ERR_OK)
   {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  esp_now_register_recv_cb(print_received_msg);
+  ESPNow.reg_recv_cb(print_received_msg);
 }
 
 void loop() {}
