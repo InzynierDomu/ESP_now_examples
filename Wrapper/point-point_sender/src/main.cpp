8c154e550a14d3dfadc5fbf7f634eec0b6be74d9
Inżynier Domu <inzynier.domu@gmail.com>
2025-03-14 11:37:29 +0100

    clear

--- Wrapper/point-point_sender/src/main.cpp
+++ Wrapper/point-point_sender/src/main.cpp
@@ -7,14 +7,14 @@
 #endif
 #include "ESPNowW.h"
 
-uint8_t receiver_mac[] = {0x3C, 0xE9, 0x0E, 0x7F, 0x30, 0x80};
+uint8_t receiver_mac[] = {0x50, 0x02, 0x91, 0xD5, 0xF7, 0xC3};
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("ESPNow sender Demo");
 #ifdef ESP8266
-  WiFi.mode(WIFI_STA); // MUST NOT BE WIFI_MODE_NULL
+  WiFi.mode(WIFI_STA);
 #elif ESP32
   WiFi.mode(WIFI_MODE_STA);
 #endif
@@ -25,8 +25,8 @@ void setup()
 
 void loop()
 {
-  static uint8_t a = 0;
-  delay(100);
+  uint8_t a = 8; // dummy value
+  delay(2000);
   ESPNow.send_message(receiver_mac, &a, 1);
   Serial.println(a++);
 }
